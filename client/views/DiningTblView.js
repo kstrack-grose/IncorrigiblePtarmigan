55f0846f053ad62a32208bdb62a6d38ebd269207
Kiri Strack-Grose <kiristrackgrose@gmail.com>
2015-08-06 16:10:00 -0700

    (doc) create guest view and dining table view for backbone

--- client/views/DiningTblView.js
+++ client/views/DiningTblView.js
@@ -1,1 +1,67 @@
 /* dining table views will be populated by guest views */
+
+var DiningTblView = Backbone.View.extend({
+  //instanticate it with a collection and an ID (num)
+
+  // it will be a table of guests as table rows
+  tagName: 'table',
+
+  class: 'diningTbl',
+
+  initialize: function() {
+    // on any updates, re-render
+    this.listenTo(this.collection, 'update', this.render);
+    // render initally
+    this.render();
+  }, 
+
+  render: function() {
+    // empty out any existing entries
+    this.$el.empty();
+
+    // add headers
+    this.$el.append('<tr><th>Guest Name</th><th>Table ID</th></tr>');
+
+    //create an array of entries, each mapped to from a model
+    this.entries = this.collection.models.map(function(guest) {
+      // only include guests who are at this particular table
+      if (guest.get('diningTableId') === this.id) {
+        return new GuestView({
+          model: guest
+        });
+      }
+    });
+
+    // obtain all the html
+    var $els = this.entries.map(function(guest) {
+      return entry.$el;
+    });
+
+    // finally, append them all
+    this.$el.append($els);
+
+    // and return. just in case.
+    return this;
+  }
+
+});
+
+
+/*
+
+    this.$el.empty();
+
+    this.entries = this.collection.models.map(function(model) {
+      return new EntryView({
+        model: model
+      });
+    });
+
+    var $els = this.entries.map(function(entry) {
+      return entry.$el;
+    });
+
+    this.$el.append($els);
+
+    return this;
+*/
--- client/views/GuestView.js
+++ client/views/GuestView.js
@@ -1,1 +1,37 @@
 /* create view for a single guest in backbone */
+
+var GuestView = Backbone.View.extend({
+  className: 'guest',
+
+  // table row
+  tagname: 'tr',
+
+  //template with two data fields within a table row
+  template: _.template('<td> <%= guestName %> </td> <td> <%= diningTableId %> </td>'),
+
+  events: {
+    'click': 'clickAction'
+  },
+
+  initialize: function() {
+    // on any change, render the entry view again
+    this.listenTo(this.model, 'change', this.render);
+    // also, just render to begin wtih
+    this.render();
+  },
+
+  render: function() {
+    // creat the html
+    var html = this.template({
+      guestName = this.model.get('guestName'),
+      diningTableId = this.model.get('diningTableId')
+    });
+    // and attach it to the table row!
+    this.$el.html(html);
+  },
+
+  clickAction: function() {
+    /* swap or edit or w/e */
+  }
+
+});
